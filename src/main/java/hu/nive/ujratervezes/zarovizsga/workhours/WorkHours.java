36b0c3de836b351dadf5424c0b0bddc148bb36ec
drprery <kpusztai777@gmail.com>
2021-03-16 19:01:08 +0100

    Záróvizsga - workhours

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -0,0 +1,34 @@
+package hu.nive.ujratervezes.zarovizsga.workhours;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class WorkHours {
+    private static final int NAME = 0;
+    private static final int HOUR = 1;
+    private static final int DATE = 2;
+
+    public String minWork(String file) {
+        try (BufferedReader br = Files.newBufferedReader(Path.of(file))) {
+            String line = "";
+            int minHours = Integer.MAX_VALUE;
+            String minWorker = null;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                int hour = Integer.parseInt(parts[HOUR]);
+
+                if (hour < minHours) {
+                    minWorker = parts[NAME] + ": " + parts[DATE];
+                }
+            }
+
+            return minWorker;
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file!");
+        }
+    }
+}
